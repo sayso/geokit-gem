02dfd9ec7d61b82469009ea667aaf7aa0810186a
bartes <bartosz.knapik@llp.pl>
2011-02-10 10:16:42 +0100

    added yajl version require - fix for yajl bug

--- lib/geokit/geocoders.rb
+++ lib/geokit/geocoders.rb
@@ -2,6 +2,7 @@ require 'timeout'
 require 'logger'
 require 'yajl'
 require 'uri'
+require 'yajl/version'
 require 'yajl/http_stream'
 require 'i18n'
 require 'active_support'
@@ -18,14 +19,14 @@ module Geokit
   class TooManyQueriesError < GeocoderServiceError; end
 
   module Geocoders
-    @@request_timeout = nil    
+    @@request_timeout = nil
     @@google = 'REPLACE_WITH_YOUR_GOOGLE_KEY'
     @@logger = Logger.new(STDOUT)
     @@logger.level = Logger::INFO
     @@domain = nil
-    
+
     def self.__define_accessors
-      class_variables.each do |v| 
+      class_variables.each do |v|
         sym = v.to_s.delete("@").to_sym
         unless self.respond_to? sym
           module_eval <<-EOS, __FILE__, __LINE__
@@ -40,7 +41,7 @@ module Geokit
               end
               value
             end
-          
+
             def self.#{sym}=(obj)
               @@#{sym} = obj
             end
@@ -50,14 +51,14 @@ module Geokit
     end
 
     __define_accessors
-    
+
     # -------------------------------------------------------------------------------------------
     # Geocoder Base class -- every geocoder should inherit from this
-    # -------------------------------------------------------------------------------------------    
-    
+    # -------------------------------------------------------------------------------------------
+
     # The Geocoder base class which defines the interface to be used by all
     # other geocoders.
-    class Geocoder   
+    class Geocoder
 
       # Main method which calls the do_geocode template method which subclasses
       # are responsible for implementing.  Returns a populated GeoLoc or an
@@ -69,18 +70,18 @@ module Geokit
           converted_address = address
         end
         do_geocode(converted_address, options)
-      end  
+      end
       # Main method which calls the do_reverse_geocode template method which subclasses
       # are responsible for implementing.  Returns a populated GeoLoc or an
       # nil one with a failed success code.
       def self.reverse_geocode(latlng)
         do_reverse_geocode(latlng)
       end
-      
+
       # Call the geocoder service using the timeout if configured.
       def self.call_geocoder_service(url)
-        Timeout::timeout(Geokit::Geocoders::request_timeout) { return self.do_get(url) } if Geokit::Geocoders::request_timeout   
-        logger.info "Getting geocode from #{url}" 
+        Timeout::timeout(Geokit::Geocoders::request_timeout) { return self.do_get(url) } if Geokit::Geocoders::request_timeout
+        logger.info "Getting geocode from #{url}"
         return self.do_get(url)
       rescue TimeoutError, Timeout::Error
         raise Geokit::TimeoutError
@@ -97,14 +98,14 @@ module Geokit
 
       protected
 
-      def self.logger() 
+      def self.logger()
         Geokit::Geocoders::logger
       end
-      
+
       private
-      
+
       # Wraps the geocoder call around a proxy if necessary.
-      def self.do_get(url) 
+      def self.do_get(url)
         uri = URI.parse(url)
         result = nil
         Yajl::HttpStream.get(uri, {:check_utf8 => false}) do |data|
@@ -112,7 +113,7 @@ module Geokit
         end
         result
       end
-      
+
     end
-  end  
+  end
 end
